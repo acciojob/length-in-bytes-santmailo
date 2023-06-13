ada011abb8d8ba9653cf9430791fcc14fd7ad263
Sunil Sharma <76843848+santmailo@users.noreply.github.com>
2023-06-13 22:05:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const byteSize = (str) => {
   // write your code here
+	  // Convert the string to a byte array.
+  const bytes = new Uint8Array(str);
+
+  // Return the length of the byte array.
+  return bytes.length;
 };
 
 // Do not change the code below
